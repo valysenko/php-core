da60b6df0244c16bd6cbbaf67d27a2953fb9b983
Lysenko Vladyslav <stalkervlad2011@ukr.net>
2014-04-22 19:25:08 +0300

    improved script.php

--- script.php
+++ script.php
@@ -1,5 +1,5 @@
 <?php
-$array = array("HTC,SAMSUNG,APPLE,LG,NOKIA,MOTOROLA");
+$array = array("HTC","SAMSUNG","APPLE","LG","NOKIA","MOTOROLA");
 echo "<ul>";
 for($i=0;$i<count($array);$i++){
 	echo("<li>".$array[$i]."</li>");
